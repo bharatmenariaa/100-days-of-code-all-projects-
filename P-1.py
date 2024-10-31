a6bf5d3cdbe84c9fe3a654a590ac58a3b1c62a35
bharatmenariaa <menariabharat34@gmail.com>
2024-10-31 17:02:53 +0530

    Add files via upload

---
+++ P-1.py
@@ -0,0 +1,8 @@
+'''Day 1 project -  Generate Band Name using city of birth and pet name'''
+
+
+
+print("Welcome to Band Name Generator")
+street = input("Enter your Birth palce.\n")
+pet = input("Enter your pet name.\n")
+print("Your Band Name could be :"+" "+street+" "+pet)
